734a42cbe1c84013111909d61c5144f5c6388327
s570908 <songohyoung@gmail.com>
2020-11-20 20:58:51 +0900

    To use kyungoh server, under modifying, not yet debgged

---
+++ client/src/Components/PassengerHome/PassengerHomeQueries1.ts
@@ -0,0 +1,64 @@
+import { gql } from "apollo-boost";
+
+export const REQUEST_RIDE = gql`
+	mutation RequestRide(
+		$pickUpAddress: String!
+		$pickUpLat: Float!
+		$pickUpLng: Float!
+		$dropOffAddress: String!
+		$dropOffLat: Float!
+		$dropOffLng: Float!
+		$duration: String!
+		$distance: String!
+		$price: Float!
+		//$rideImage: String!
+	) {
+		RequestRide(
+			pickUpAddress: $pickUpAddress
+			pickUpLat: $pickUpLat
+			pickUpLng: $pickUpLng
+			dropOffAddress: $dropOffAddress
+			dropOffLat: $dropOffLat
+			dropOffLng: $dropOffLng
+			duration: $duration
+			distance: $distance
+			price: $price
+			//rideImage: $rideImage
+		) {
+			ok
+			error
+			ride {
+				id
+			}
+		}
+	}
+`;
+
+export const GET_NEARBY_DRIVERS = gql`
+	query GetNearbyDrivers {
+		GetNearbyDrivers {
+			ok
+			error
+			drivers {
+				id
+				fullName
+				profilePhoto
+				lastLat
+				lastLng
+			}
+		}
+	}
+`;
+
+export const GET_RIDE_BY_ID = gql`
+	query GetRideById($rideId: Int!) {
+		GetRideById(rideId: $rideId) {
+			ok
+			error
+			ride {
+				id
+				status
+			}
+		}
+	}
+`;
---
+++ client/src/Routes/Home/HomePresenter1.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import DriverHome from "../../Components/DriverHome";
+import Maps from "../../Components/Maps";
+import PassengerHome from "../../Components/PassengerHome";
+import { GetMyProfile as GetCurrentUser} from "../../types/api";
+import { ICoords } from "../../utils/mapHelpers";
+import * as S from "./HomeStyle";
+
+interface IProps {
+	map?: google.maps.Map<Element>;
+	userMarker?: google.maps.Marker;
+	userCoords: ICoords;
+	userData?: GetMyProfile;
+	setMap: React.Dispatch<
+		React.SetStateAction<google.maps.Map<Element> | undefined>
+	>;
+}
+
+const HomePresenter: React.FC<IProps> = ({
+	map,
+	userMarker,
+	userCoords,
+	userData: { GetCurrentUser: { user = {} } = {} } = {},
+	setMap
+}) => {
+	return (
+		<S.Contaier>
+			{user!.isDriving ? (
+				<DriverHome />
+			) : (
+				<PassengerHome
+					map={map}
+					userMarker={userMarker}
+					userCoords={userCoords}
+				/>
+			)}
+			<Maps setMap={setMap} isHome={true} />
+		</S.Contaier>
+	);
+};
+
+export default HomePresenter;
